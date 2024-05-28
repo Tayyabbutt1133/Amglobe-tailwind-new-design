415b669f7791370c5c0ba6b8c33dea0784def89e
Tayyeb butt <coolpearl03@gmail.com>
2024-05-28 16:49:26 +0500

    final commit

--- amglobetailwind/src/components/Footer/Footer.jsx
+++ amglobetailwind/src/components/Footer/Footer.jsx
@@ -9,8 +9,14 @@ import circle from '../../assets/Images/reserveCircle.svg';
 // grid grid-cols-1 sm:grid-cols-3
 const Footer = () => {
   return (
-    <div className="footermain">
-      <div className="footer-container flex lg:justify-end justify-center md:gap-20 lg:gap-32 sm:gap-24 gap-12 sm:flex-row flex-col  text-center sm:text-start">
+    // <div className="footermain">
+
+
+      <div className="footer-container">
+
+        
+
+        <div className='footerInner-Div  flex lg:justify-end justify-center md:gap-20 lg:gap-32 sm:gap-24 gap-12 sm:flex-row flex-col  text-center sm:text-start p-28'>
         <div className="footerinner">
           <ul className=' md:text-[1em] text-[0.8em]'>
             <li><span className='footer-span text-white text-[1.2em]'>Sitemap</span></li>
@@ -39,13 +45,20 @@ const Footer = () => {
             <a href="#"><img src={youtube}  className=' lg:w-10 xs:w-8 w-96' alt="" /></a>
             <a href="#"><img src={fb}       className=' lg:w-10 xs:w-8 w-96' alt="" /></a>
           </div>
-        </div>
-      </div>
-      <div className='footer-reserve'>
+          </div>
+          </div>
+         <div className='footer-reserve mx-32'>
         <p className='footer-reserve-text xs:text-sm text-[0.6em]'>© 2024 AM GLOBAL. All rights reserved.</p>
         <img src={circle} alt="" className='footer-reserve-circle md:block hidden' />
+        </div> 
+         
+
+
       </div>
-    </div>
+
+
+      // </div>
+   
   )
 }
 
